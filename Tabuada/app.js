9f1827dc45a7a3a7a00d8c036b383b3f074d6084
Daniel Monteiro <danielmonteirodcarvalho@gmail.com>
2023-04-28 17:15:22 -0300

    Add files via upload

---
+++ Tabuada/app.js
@@ -0,0 +1,10 @@
+function tabuada() {
+    var num = parseInt(document.getElementById("num").value);
+    var resposta = document.getElementById("resposta");
+    var tabuada="";
+
+    for (var count = 1; count <= 10; count++)
+        tabuada += num + "x" + count + "=" + num * count + "<br>";
+
+    resposta.innerHTML = tabuada;
+}
